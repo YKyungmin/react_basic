a9a7b609d9a72acb7cc34bcac810c14fc90fb85a
YKyungmin <xcv556865@gmail.com>
2023-10-05 13:45:59 +0900

    모달에 유튜브 영상 출력

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,11 +1,67 @@
 import Layout from '../../common/layout/Layout';
-//import styles from './Department.module.scss';
-//import clsx from 'clsx';
+import Modal from '../../common/modal/Modal';
+import './Youtube.scss';
+import { useEffect, useState } from 'react';
+/*
+	리액트는 단방향 데이터 바인딩
+	- 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
+	- props전달, children 전달
+	
+	리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
+	- 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
+	- 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
+
+	useRef로 jsx는 참조 객체에 담을 수 있음
+*/
 
 export default function Youtube() {
+	const [Youtube, setYoutube] = useState([]);
+	const [IsModal, setIsModal] = useState(false);
+
+	const fetchYoutube = () => {
+		const api_key = process.env.REACT_APP_YOUTUBE_API;
+		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+		const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const num = 5;
+		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+		fetch(resultURL)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json.items);
+				setYoutube(json.items);
+			});
+	};
+	useEffect(() => {
+		fetchYoutube();
+	}, []);
+
 	return (
-		<Layout title={'Youtube'}>
-			<p>유튜브 페이지입니다.</p>
-		</Layout>
+		<>
+			<Layout title={'Youtube'}>
+				{Youtube.map((data, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{data.snippet.title}</h2>
+							<p>{data.snippet.description}</p>
+							<div className='pic' onClick={() => setIsModal(true)}>
+								<img
+									src={data.snippet.thumbnails.standard.url}
+									alt={data.title}
+								/>
+							</div>
+						</article>
+					);
+				})}
+			</Layout>
+			{IsModal && (
+				<Modal setIsModal={setIsModal}>
+					<iframe
+						src={`https://www.youtube.com/embed/${Youtube[0].snippet.resourceId.videoId}`}
+						title='youtube'
+					></iframe>
+				</Modal>
+			)}
+		</>
 	);
 }
