f7b2eaf2505d5a90f782649c50f93d0012c411c3
YKyungmin <xcv556865@gmail.com>
2023-10-06 14:16:46 +0900

    메인전송 완료후 폼요소 초기화

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 import emailjs from '@emailjs/browser';
@@ -10,7 +9,7 @@ export default function Contact() {
 	const view = useRef(null);
 	const instance = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
-	const [Index, setIndex] = useState(2);
+	const [Index, setIndex] = useState(0);
 	const [IsMap, setIsMap] = useState(true);
 
 	const { kakao } = window;
@@ -94,10 +93,21 @@ export default function Contact() {
 			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
+	const resetForm = () => {
+		const [nameKey, nameValue, emailKey, emailValue, msgKey, msgValue] =
+			form.current.children;
+
+		nameValue.value = '';
+		emailValue.value = '';
+		msgValue.value = '';
+	};
+
 	//form mail 기능함수
 	const sendEmail = (e) => {
 		e.preventDefault();
 
+		//sendForm메서드는 각 키값을 문자열로만 인수로 전달되도록 type지정되어 있기 때문에
+		//변수를 `${}`로 감싸서 문자형식으로 전달
 		emailjs
 			.sendForm(
 				'service_9ux69ff',
@@ -108,9 +118,13 @@ export default function Contact() {
 			.then(
 				(result) => {
 					alert('문의내용이 메일로 발송되었습니다.');
+					console.log(result);
+					resetForm();
 				},
 				(error) => {
 					alert('문의내용 전송에 실패했습니다.');
+					console.log(error);
+					resetForm();
 				}
 			);
 	};
