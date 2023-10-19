95213923661b4e8c2b3e64e94fd364a95d966127
YKyungmin <xcv556865@gmail.com>
2023-10-19 16:11:58 +0900

    header안눌리는 버그 수정

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -27,6 +27,7 @@ export default function Community() {
 			data: new Date(),
 		},
 	];
+
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
 		if (data) return JSON.parse(data);
@@ -36,6 +37,7 @@ export default function Community() {
 	const refTextarea = useRef(null);
 	const refEditInput = useRef(null);
 	const refEditTextarea = useRef(null);
+
 	const [Posts, setPosts] = useState(getLocalData());
 	const [Allowed, setAllowed] = useState(true);
 	console.log(Posts);
@@ -49,7 +51,6 @@ export default function Community() {
 			resetForm();
 			return alert('제목과 본문을 모두 입력하세요.');
 		}
-		//기존 Posts 배열값을 Deep copy해서 가져온뒤, 그 뒤에 추가로 방금 입력한 객체를 배열에 추가
 		setPosts([
 			{
 				title: refInput.current.value,
@@ -63,20 +64,14 @@ export default function Community() {
 
 	const deletePost = (delIndex) => {
 		if (window.confirm('정말 해당 게시글을 삭제하겠습니까?')) {
-			//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
 			setPosts(Posts.filter((_, idx) => delIndex !== idx));
 		}
 	};
 
-	//해당 글을 수정모드로 변경시키는 함수
 	const enableUpdate = (editIndex) => {
-		//수정모드 함수 호출시 Allowed가 true가 아니면 return으로 함수 강제 종료
 		if (!Allowed) return;
-		//일단 수정모드에 진입하면 강제로 Allowed값을 false로 변경해서 다른 글 수정모드 진입금지 처리
 		setAllowed(false);
 		setPosts(
-			//Posts 배열값을 반복돌면서 인수로 전달된 수정할 포스트의 순번값과 현재 반복도는 배열의 포스트 순번값이 일치하면
-			//해당 글을 수정처리해야되므로 해당 객체에 enableUpdate=true값을 추가
 			Posts.map((post, idx) => {
 				if (editIndex === idx) post.enableUpdate = true;
 				return post;
@@ -95,10 +90,7 @@ export default function Community() {
 		);
 	};
 
-	//실제 글 수정하는 함수
 	const updatePost = (updateIndex) => {
-		//setPosts로 기존 Post배열같은 덮어쓰기해서 변경
-		//리액트에서는 참조형 자료는 무조건 배열값을 Deep copy한뒤 변경
 		setPosts(
 			Posts.map((post, idx) => {
 				if (updateIndex === idx) {
@@ -117,6 +109,7 @@ export default function Community() {
 	return (
 		<Layout title={'Community'}>
 			<div className='inputBox'>
+				<h2>Comment</h2>
 				<input ref={refInput} type='text' placeholder='제목을 입력하세요.' />
 				<br />
 				<textarea
@@ -156,12 +149,7 @@ export default function Community() {
 										ref={refEditInput}
 									/>
 									<br />
-									<textarea
-										//react에서 value속성을 적용하려면 무조건 onChange이벤트 연결 필수
-										//onChange이벤트 연결하지 않을때에는 value가닌 defaultValue속성 적용
-										defaultValue={post.content}
-										ref={refEditTextarea}
-									/>
+									<textarea defaultValue={post.content} ref={refEditTextarea} />
 								</div>
 								<nav className='btnSet'>
 									<button onClick={() => disableUpdate(idx)}>Cancel</button>
@@ -177,15 +165,13 @@ export default function Community() {
 							</article>
 						);
 					} else {
-						//출력 모드 렌더링
-
 						return (
 							<article key={idx}>
 								<div className='txt'>
 									<h2>{post.title}</h2>
-									<p>{post.content}</p>
-									<p>{`글 작성일 : ${year}-${month}-${date}`}</p>
-									<p>{`글 작성시간 : ${hour}:${min}:${sec}`}</p>
+									<p className='txt_content'>{post.content}</p>
+									<p className='txt_date '>{`Date : ${year} / ${month} / ${date}`}</p>
+									<p className='txt_time '>{`Time : ${hour}:${min}:${sec}`}</p>
 								</div>
 
 								<nav className='btnSet'>
@@ -201,8 +187,13 @@ export default function Community() {
 	);
 }
 
-//아직 데이터베이스를 배우진 않았지만 CRUD기능 구현하고 싶어서 로컬저장소를 활용해서 만들어 봤다.
-//이슈사항으로는 시간값을 가져왔는데 로컬저장소에 글이 저장되는 시점의 시간을 표준시로 저장을 해서 현재시간보다 9시간이 늦은 시간으로 출력되는 문제가 있었다.
-//시간값을 변경하려고 보니 JSON.parse로 객체형태로 시간을 불러와져서 split 메서드를 쓸수가 없는데 이유를 몰라서 삽질했다.
-//객체형태로 변환된 값을 다시 stringify로 문자화시킨다음에 split으로 문자값 가공하고 다시 화면에 출력
-//두번째 이슈사항으로 친구컴퓨터로 내 작업물을 확인해보니 해당 브라우저에는 저장된 데이터가 없어서 커뮤니티 페이지가 빈화면으로 출력되는 이슈 --> 로컬저장소에 값이 없을때 더미 데이터가 출력되도록 했다.
+/*
+아직 데이터베이스를 배우진 않았지만 CRUD기능 구현하고 싶어서 로컬저장소를 활용해서 만들어 봤다.
+이슈사항으로는 시간값을 가져왔는데 로컬저장소에 글이 저장되는 시점의 시간을 표준시로 저장을 해서 현재시간보다 9시간이 늦은 시간으로 출력되는 문제가 있었다.
+시간값을 변경하려고 보니 JSON.parse로 객체형태로 시간을 불러와져서 split 메서드를 쓸수가 없는데 이유를 몰라서 삽질했습니다.
+
+
+객체형태로 변환된 값을 다시 stringify로 문자화시킨다음에 split으로 문자값 가공하고 다시 화면에 출력
+두번째 이슈사항으로 친구컴퓨터로 내 작업물을 확인해보니 해당 브라우저에는 저장된 데이터가 없어서 커뮤니티 페이지가 빈화면으로 출력되는 이슈 --> 로컬저장소에 값이 없을때 더미 데이터가 출력되도록 했다.
+
+*/
